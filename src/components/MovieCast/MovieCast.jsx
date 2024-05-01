7ab05dbfa4e38439f496905947c369deb7f52cff
bondalina <142501520+bondalina@users.noreply.github.com>
2024-05-01 18:53:50 +0300

    update movieCast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -26,7 +26,8 @@ const MovieCast = () => {
 
     fetchCast();
   }, [movieId]);
-
+  const defaultImg =
+    "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
   return (
     <div>
       {loading && <p>Loading cast...</p>}
@@ -36,7 +37,11 @@ const MovieCast = () => {
           {cast.map((actor) => (
             <li key={actor.id}>
               <img
-                src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
+                src={
+                  actor.profile_path
+                    ? `https://image.tmdb.org/t/p/w500/${actor.profile_path}`
+                    : defaultImg
+                }
                 alt={actor.name}
                 width={200}
               />
